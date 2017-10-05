fb93a2a1f6a3a74b8d02aa5ad378bf4bfbed96b7
wlybq <chenmingjie121@163.com>
2017-10-05 22:26:10 +0800

    改正build出文件不能正常打开问题

--- src/routes.config.js
+++ src/routes.config.js
@@ -1,11 +1,6 @@
-import Vue from 'vue'
-import App from './App.vue'
 import VueRouter from "vue-router";
-// import router from "./routes.config.js";
 import product from "./components/product/product.vue"
 import resolve from "./components/resolve/resolve.vue"
-
-Vue.use(VueRouter);
 
 const routes = [  
     { path: '/product/:id', component: product },  
@@ -14,12 +9,4 @@ const routes = [
 
 const router = new VueRouter({ routes: routes });  
 
-const app = new Vue({ 
-	router: router,
-	render: h => h(App),
-	created() {
-	  	layui.use(['element'], function () {
-	  		const element = layui.element;
-	  	});
-	}
- }).$mount('#app'); 
+export { router }
